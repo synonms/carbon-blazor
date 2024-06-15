45bf3c7358765815a0c4f8a141e7cac1d619f40d
synonms <syno.nms74@outlook.com>
2024-06-15 12:38:21 +0100

    Started updating radio button

---
+++ Synonms.CarbonBlazor/Models/RadioButtonItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Synonms.CarbonBlazor.Models;
+
+public class RadioButtonItem
+{
+    public RadioButtonItem(string label, string value)
+    {
+        Label = label;
+        Value = value;
+    }
+
+    public string Label { get; }
+    
+    public string Value { get; }
+
+    public string Id { get; init;  } = Guid.NewGuid().ToString();
+
+    public static RadioButtonItem Create(string value) => new(value, value);
+    public static RadioButtonItem Create(string label, string value) => new(label, value);
+}
